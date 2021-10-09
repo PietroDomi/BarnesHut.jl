8fa67570b2b17facf1e38643dd8deb2d8f1ca835
PIETRO DOMINIETTO (pietro.dominietto@studbocconi.it) <3054552@dsba.sm.unibocconi.it>
2021-10-10 00:37:52 +0200

    minor fix

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -25,8 +25,8 @@ for n in N
     @info "Starting brute simualation"
     hist_brute, bench_time_brute = @timed simulationBrute(galaxy,T,3600.;timing=true);
     mean_time_brute = sum(bench_time_brute)/length(bench_time_brute)
-    write(g,"Brute, $T, $n")
-    write(g,"$(bench_time_brute)")
+    write(g,"Brute, $T, $n\n")
+    write(g,"$(bench_time_brute)\n")
     write(f,"$T,$n,BF,_,$(mean_time_brute)\n")
     @info "Avg step Brute Simulation in $(mean_time_brute)\n"
     
@@ -34,16 +34,16 @@ for n in N
     hist_tree1, bench_time_tree1 = simulationTree(galaxy,T,3600.,0.4;timing=true);
     mean_time_tree1 = sum(bench_time_tree1)/length(bench_time_tree1)
     write(f,"$T,$n,BH,0.4,$(mean_time_tree1)\n")
-    write(g,"BH, T=$T, n=$n, θ=0.4")
-    write(g,"$(bench_time_tree1)")
+    write(g,"BH, T=$T, n=$n, θ=0.\n4")
+    write(g,"$(bench_time_tree1)\n")
     @info "Avg step Tree Simulation in $(mean_time_tree1)\n"
     
     @info "Starting tree simulation with θ=0.8"
     hist_tree2, bench_time_tree2 = @timed simulationTree(galaxy,T,3600.,0.8;timing=true);
     mean_time_tree2 = sum(bench_time_tree2)/length(bench_time_tree2)
     write(f,"$T,$n,BH,0.8,$(mean_time_tree2)\n")
-    write(g,"BH, T=$T, n=$n, θ=0.8")
-    write(g,"$(bench_time_tree2)")
+    write(g,"BH, T=$T, n=$n, θ=0.\n8")
+    write(g,"$(bench_time_tree2)\n")
     @info "Avg step Tree Simulation in $(mean_time_tree2)\n"
 
 end
