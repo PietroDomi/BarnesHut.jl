c12e5d27f0677904257c08c7e97fb6bfe04842c7
PietroDomi <pietro.dominietto@gmail.com>
2021-10-09 19:15:45 +0200

    testing without plots

--- test/galaxy-clash.jl
+++ test/galaxy-clash.jl
@@ -1,10 +1,3 @@
-# if length(LOAD_PATH) == 3
-#     # add here the path to the working directory of the module
-#     push!(LOAD_PATH,"C:/Users/PietroDomi/OneDrive - Università Commerciale Luigi Bocconi/Documenti/GitHub/barnes-hut-julia")
-# end
-
-# using Plots
-# using BenchmarkTools
 using BarnesHut
 
 println("Starting Galaxy clash simulation")
@@ -13,20 +6,19 @@ N = 500
 T = 24*30*6
 # const plotLimits = nothing
 plotLimits = [-20.,40.]
+animation = true
 fps = 60
 
-galaxy = galaxy_start(N÷2,[0.,0.],[10.,10.],10.)
-galaxy2 = galaxy_start(N÷2,[20.,20.],[-10.,-10.],10.)
-# galaxy = cloud_start(N,[0.,0.])
+galaxy = galaxy_start(N÷2,[0.,0.],[10.,10.],20.)
+galaxy2 = galaxy_start(N÷2,[20.,20.],[-10.,-10.],20.)
 
 append!(galaxy,galaxy2)
 
-# display(BHNbody.viewStart(galaxy))
+gal_hist = simulationTree(galaxy,T,3600.,0.5)
 
-gal_hist = simulationTree(galaxy,T,3600.,0.5,false)
-
-anim_tree = build_animation(gal_hist,plotLimits,plotLimits,24,clash=true,N=N÷2)
-
-println("Animation computed.\nBuilding gif...")
-
-gif(anim_tree, "gifs/galaxy-clash-n$N-t$T-fps$fps.gif", fps=fps);
+if animation
+    using Plots
+    anim_tree = build_animation(gal_hist,plotLimits,plotLimits,24,clash=true,N=N÷2)
+    println("Animation computed.\nBuilding gif...")
+    gif(anim_tree, "test/gifs/galaxy-clash-n$N-t$T-fps$fps.gif", fps=fps);
+end
--- test/single-galaxy.jl
+++ test/single-galaxy.jl
@@ -1,30 +1,22 @@
-# if length(LOAD_PATH) == 3
-#     # add here the path to the working directory of the module
-#     push!(LOAD_PATH,"C:/Users/PietroDomi/OneDrive - Università Commerciale Luigi Bocconi/Documenti/GitHub/barnes-hut-julia")
-# end
-
-# using Plots
-# using BenchmarkTools
-# using BarnesHut
+using BarnesHut
 
 println("Starting Single Galaxy simulation")
 
 N = 500
-T = 24*30*4
+T = 24*30*6
 # const plotLimits = nothing
 plotLimits = [-20.,20.]
+animation = true
 fps = 60
 
 galaxy = galaxy_start(N÷2,[0.,0.],[0.1,0.1],10.)
 # galaxy = cloud_start(N,[0.,0.])
 
+gal_hist = simulationTree(galaxy,T,3600.,0.5)
 
-# display(BHNbody.viewStart(galaxy))
-
-gal_hist = simulationTree(galaxy,T,3600.,0.5,false)
-
-anim_tree = build_animation(gal_hist,plotLimits,plotLimits,24);
-
-println("Animation computed.\nBuilding gif...")
-
-gif(anim_tree, "gifs/galaxy-tree-n$N-t$T-fps$fps.gif", fps=fps);
+if animation
+    using Plots
+    anim_tree = build_animation(gal_hist,plotLimits,plotLimits,24);
+    println("Animation computed.\nBuilding gif...")
+    gif(anim_tree, "gifs/galaxy-tree-n$N-t$T-fps$fps.gif", fps=fps);
+end
